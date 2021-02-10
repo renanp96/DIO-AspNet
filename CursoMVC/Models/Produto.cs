05dcd213d7f1db6c867ca1ea4306df9a34c80900
Renan Paganini <renanpa1996@gmail.com>
2021-02-10 11:29:00 -0300

    Projeto base finalizado, iniciando o projeto de APIs

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models {
     public class Produto {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa!")]
         public string Descricao { get; set; }
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
